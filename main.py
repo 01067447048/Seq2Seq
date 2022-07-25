b96c77a5d571ef1486ca0d0e879f027db56d2a4a
jaehyeon <morphine.coder@gmail.com>
2022-07-25 19:10:01 +0900

    주석 제거.

--- main.py
+++ main.py
@@ -107,98 +107,98 @@ decoder_target = to_categorical(decoder_target)
 
 # Training Start
 
-# encoder_inputs = Input(shape=(None, src_vocab_size))
-# encoder_lstm = LSTM(units=256, return_state=True)
-#
-# # encoder_outputs은 여기서는 불필요
-# encoder_outputs, state_h, state_c = encoder_lstm(encoder_inputs)
-#
-# # LSTM은 바닐라 RNN과는 달리 상태가 두 개. 은닉 상태와 셀 상태.
-# encoder_states = [state_h, state_c]
-#
-# decoder_inputs = Input(shape=(None, tar_vocab_size))
-# decoder_lstm = LSTM(units=256, return_sequences=True, return_state=True)
-#
-# # decoder 에게 encoder 의 은닉 상태, 셀 상태를 전달.
-# # decoder 는 encoder 의 마지막 은닉 상태를 초기 은닉 상태로 사용
-#
-# decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
-#
-# decoder_softmax_layer = Dense(tar_vocab_size, activation='softmax')
-# decoder_outputs = decoder_softmax_layer(decoder_outputs)
-#
-# model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
-# model.compile(optimizer="rmsprop", loss="categorical_crossentropy")
-#
-# model.fit(x=[encoder_input, decoder_input], y=decoder_target, batch_size=64, epochs=40, validation_split=0.2)
-#
-# # Training Finish
-#
-# # 번역기로 동작.
-#
-# encoder_model = Model(inputs=encoder_inputs, outputs=encoder_states)
-#
-# # 이전 시점의 상태들을 저장하는 텐서
-# decoder_state_input_h = Input(shape=(256,))
-# decoder_state_input_c = Input(shape=(256,))
-# decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
-#
-# # 문장의 다음 단어를 예측하기 위해서 초기 상태(initial_state)를 이전 시점의 상태로 사용.
-# # 뒤의 함수 decode_sequence()에 동작을 구현 예정
-# decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
-#
-# # 훈련 과정에서와 달리 LSTM의 리턴하는 은닉 상태와 셀 상태를 버리지 않음.
-# decoder_states = [state_h, state_c]
-# decoder_outputs = decoder_softmax_layer(decoder_outputs)
-# decoder_model = Model(inputs=[decoder_inputs] + decoder_states_inputs, outputs=[decoder_outputs] + decoder_states)
-#
-# # 단어로부터 인덱스를 얻는게 아니라 인덱스로부터 단어를 얻을 수 있도록 index_to_src index_to_tar 생성.
-# index_to_src = dict((i, char) for char, i in src_to_index.items())
-# index_to_tar = dict((i, char) for char, i in tar_to_index.items())
-#
-#
-# def decode_sequence(input_seq):
-#     # 입력으로부터 인코더의 상태를 얻음
-#     states_value = encoder_model.predict(input_seq)
-#
-#     # <sos>에 해당하는 원-핫 벡터 생성
-#     target_seq = np.zeros((1, 1, tar_vocab_size))
-#     target_seq[0, 0, tar_to_index['\t']] = 1.
-#
-#     stop_condition = False
-#     decoded_sentence = ""
-#
-#     # stop_condition이 True가 될 때까지 루프 반복
-#     while not stop_condition:
-#         # 이점 시점의 상태 states_value를 현 시점의 초기 상태로 사용
-#         output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
-#
-#         # 예측 결과를 문자로 변환
-#         sampled_token_index = np.argmax(output_tokens[0, -1, :])
-#         sampled_char = index_to_tar[sampled_token_index]
-#
-#         # 현재 시점의 예측 문자를 예측 문장에 추가
-#         decoded_sentence += sampled_char
-#
-#         # <eos>에 도달하거나 최대 길이를 넘으면 중단.
-#         if (sampled_char == '\n' or
-#                 len(decoded_sentence) > max_tar_len):
-#             stop_condition = True
-#
-#         # 현재 시점의 예측 결과를 다음 시점의 입력으로 사용하기 위해 저장
-#         target_seq = np.zeros((1, 1, tar_vocab_size))
-#         target_seq[0, 0, sampled_token_index] = 1.
-#
-#         # 현재 시점의 상태를 다음 시점의 상태로 사용하기 위해 저장
-#         states_value = [h, c]
-#
-#     return decoded_sentence
-#
-#
-# for seq_index in [3, 50, 100, 300, 1001]:  # 입력 문장의 인덱스
-#     input_seq = encoder_input[seq_index:seq_index + 1]
-#     decoded_sentence = decode_sequence(input_seq)
-#     print(35 * "-")
-#     print('입력 문장:', lines.src[seq_index])
-#     print('정답 문장:', lines.tar[seq_index][2:len(lines.tar[seq_index]) - 1])  # '\t'와 '\n'을 빼고 출력
-#     print('번역 문장:', decoded_sentence[1:len(decoded_sentence) - 1])  # '\n'을 빼고 출력
+encoder_inputs = Input(shape=(None, src_vocab_size))
+encoder_lstm = LSTM(units=256, return_state=True)
+
+# encoder_outputs은 여기서는 불필요
+encoder_outputs, state_h, state_c = encoder_lstm(encoder_inputs)
+
+# LSTM은 바닐라 RNN과는 달리 상태가 두 개. 은닉 상태와 셀 상태.
+encoder_states = [state_h, state_c]
+
+decoder_inputs = Input(shape=(None, tar_vocab_size))
+decoder_lstm = LSTM(units=256, return_sequences=True, return_state=True)
+
+# decoder 에게 encoder 의 은닉 상태, 셀 상태를 전달.
+# decoder 는 encoder 의 마지막 은닉 상태를 초기 은닉 상태로 사용
+
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+
+decoder_softmax_layer = Dense(tar_vocab_size, activation='softmax')
+decoder_outputs = decoder_softmax_layer(decoder_outputs)
+
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+model.compile(optimizer="rmsprop", loss="categorical_crossentropy")
+
+model.fit(x=[encoder_input, decoder_input], y=decoder_target, batch_size=64, epochs=40, validation_split=0.2)
+
+# Training Finish
+
+# 번역기로 동작.
+
+encoder_model = Model(inputs=encoder_inputs, outputs=encoder_states)
+
+# 이전 시점의 상태들을 저장하는 텐서
+decoder_state_input_h = Input(shape=(256,))
+decoder_state_input_c = Input(shape=(256,))
+decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+
+# 문장의 다음 단어를 예측하기 위해서 초기 상태(initial_state)를 이전 시점의 상태로 사용.
+# 뒤의 함수 decode_sequence()에 동작을 구현 예정
+decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+
+# 훈련 과정에서와 달리 LSTM의 리턴하는 은닉 상태와 셀 상태를 버리지 않음.
+decoder_states = [state_h, state_c]
+decoder_outputs = decoder_softmax_layer(decoder_outputs)
+decoder_model = Model(inputs=[decoder_inputs] + decoder_states_inputs, outputs=[decoder_outputs] + decoder_states)
+
+# 단어로부터 인덱스를 얻는게 아니라 인덱스로부터 단어를 얻을 수 있도록 index_to_src index_to_tar 생성.
+index_to_src = dict((i, char) for char, i in src_to_index.items())
+index_to_tar = dict((i, char) for char, i in tar_to_index.items())
+
+
+def decode_sequence(input_seq):
+    # 입력으로부터 인코더의 상태를 얻음
+    states_value = encoder_model.predict(input_seq)
+
+    # <sos>에 해당하는 원-핫 벡터 생성
+    target_seq = np.zeros((1, 1, tar_vocab_size))
+    target_seq[0, 0, tar_to_index['\t']] = 1.
+
+    stop_condition = False
+    decoded_sentence = ""
+
+    # stop_condition이 True가 될 때까지 루프 반복
+    while not stop_condition:
+        # 이점 시점의 상태 states_value를 현 시점의 초기 상태로 사용
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value)
+
+        # 예측 결과를 문자로 변환
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = index_to_tar[sampled_token_index]
+
+        # 현재 시점의 예측 문자를 예측 문장에 추가
+        decoded_sentence += sampled_char
+
+        # <eos>에 도달하거나 최대 길이를 넘으면 중단.
+        if (sampled_char == '\n' or
+                len(decoded_sentence) > max_tar_len):
+            stop_condition = True
+
+        # 현재 시점의 예측 결과를 다음 시점의 입력으로 사용하기 위해 저장
+        target_seq = np.zeros((1, 1, tar_vocab_size))
+        target_seq[0, 0, sampled_token_index] = 1.
+
+        # 현재 시점의 상태를 다음 시점의 상태로 사용하기 위해 저장
+        states_value = [h, c]
+
+    return decoded_sentence
+
+
+for seq_index in [3, 50, 100, 300, 1001]:  # 입력 문장의 인덱스
+    input_seq = encoder_input[seq_index:seq_index + 1]
+    decoded_sentence = decode_sequence(input_seq)
+    print(35 * "-")
+    print('입력 문장:', lines.src[seq_index])
+    print('정답 문장:', lines.tar[seq_index][2:len(lines.tar[seq_index]) - 1])  # '\t'와 '\n'을 빼고 출력
+    print('번역 문장:', decoded_sentence[1:len(decoded_sentence) - 1])  # '\n'을 빼고 출력
